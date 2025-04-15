fdced1995b60bd079c70d12435c973fb8f0fccc6
kiran <kiran>
2025-04-15 12:48:42 +1000

    arrays - slices

--- hello-world/main.go
+++ hello-world/main.go
@@ -8,7 +8,12 @@ import (
 func main() {
 	name, score := "Kiran", 85
 
+	students := []string{"Dent", "Arthur"}
+	scores := []int{50, 75}
+
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 20))
 	fmt.Println(name, ":", score)
+	fmt.Println(students[0], ":", scores[0])
+	fmt.Println(students[1], ":", scores[1])
 }
