65e5f18649e72500f663704952d15af69136048e
kiran <kiran>
2025-04-15 12:33:00 +1000

    simple types - string, number, boolean

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	name, score := "Kiran", 85
+
+	fmt.Println("Student scores")
+	fmt.Println(strings.Repeat("-", 20))
+	fmt.Println(name, ":", score)
 }
